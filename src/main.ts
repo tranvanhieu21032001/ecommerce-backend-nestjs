df9326a8639ffa4793aedf0992c7d9ac1ced1759
tranvanhieu20032001 <tranvanhieu20032001@gmail.com>
2026-04-23 22:37:13 +0700

    feat: set up swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || '3000';
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,7 +20,49 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setVersion('1.0')
+    .setDescription('API documentation for the E-commerce application')
+    .addTag('auth', 'Authentication endpoints')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-AUTH',
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        name: 'Refresh-JWT',
+        description: 'Enter refresh JWT token',
+        in: 'header',
+      },
+      'JWT-REFRESH',
+    )
+    .addServer(`http://localhost:${port}`)
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+    },
+    customSiteTitle: 'My API Docs',
+    customfavIcon: 'https://example.com/favicon.ico',
+    customCss: `
+     .swagger-ui .topbar {display: none}
+      .swagger-ui .info { margin: 50px 0; }
+      .swagger-ui .info .title {color: #4A90E2;}
+    `,
+  });
+  await app.listen(port);
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap().catch((error) => {
